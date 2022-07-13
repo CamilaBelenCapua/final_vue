d1ff77c288e31b8e8a90dc2bc8714695af133447
unknown <camilabelencapua@gmail.com>
2022-07-12 21:07:57 -0300

    terminado

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,29 +1,19 @@
 <template>
    <section class="src-components-Input">
-    <div>
-     <span>color: <input type="text" v-model="color" /></span>
-     <span>fondo: <input type="text" v-model="fondo" /> </span>
-     <span>size:<input type="number" v-model="size" /></span>
-    
-     <span> texto: 
-      <input
-        type="text"
-        v-model = "texto"
-        :style="estiloTexto(color, fondo, size)"
-        class="text-center"/>
-      </span>
+      <span>Color: <input type="text" v-model="color"/></span>
+      <span>Fondo: <input type="text" v-model="fondo"/></span>
+      <span>Size: <input type="text" v-model="size"/></span>
+      <hr>
+      <span>Texto: <input type="text" :style="style(color,fondo,size)" v-model="texto"/></span>
+      <hr>
+      <span>Cantidad de palanras: {{count}}</span>
+      <Answers></Answers>
 
-      <p>Cantidad de palabras: {{ count() }}</p>
-
-        <Answers></Answers>
-        <br>
-      </div>
   </section>
 </template>
 
 <script>
 import Answers from './Answers.vue'
-
 export default {
   name: 'src-components-Input',
   components : {
@@ -35,25 +25,27 @@ export default {
   
   data () {
     return {
-      color: "",
-      fondo: "",
-      size: "",
-      texto: "Ingrese Texto Aqui",
-      
+      color:"",
+      fondo:"",
+      size:"",
+      texto:""
     }
   },
   methods: {
-    estiloTexto(color, fondo, size) {
-      return `background: ${fondo}; color:  ${color}; width:  ${size}px `;
-    }
+      style(color,fondo,size){
+        return{
+          color: color,
+          backgroundColor: fondo,
+          fontSize: size + "px"
+        }
+      }
   },
-
   
   computed: {
-    count() {
-      let count = this.texto.split(" ").length;
-      return count;
-    },
+    count(){
+      let cantidad = this.texto.split(' ')
+      return cantidad.length -1
+    }
   },
   
 }
