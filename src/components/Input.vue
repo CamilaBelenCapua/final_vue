da14e128ab0e9959394a1c90f1dcb20d0f2c6163
unknown <camilabelencapua@gmail.com>
2022-07-12 21:02:02 -0300

    inicio

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,67 +1,20 @@
 <template>
    <section class="src-components-Input">
-     <div class="Input">
-      <h3>Color</h3>
-        <hr>
-        <br>
-          <validate tag="div">
-            <input 
-              type="text"
-              id="inputColor"
-              name="inputColor" 
-              class="form-control"
-              autocomplete="off"
-              v-model.trim="formData.color" 
-            />
-          </validate>
+    <div>
+     <span>color: <input type="text" v-model="color" /></span>
+     <span>fondo: <input type="text" v-model="fondo" /> </span>
+     <span>size:<input type="number" v-model="size" /></span>
+    
+     <span> texto: 
+      <input
+        type="text"
+        v-model = "texto"
+        :style="estiloTexto(color, fondo, size)"
+        class="text-center"/>
+      </span>
 
-          <h3>Fondo</h3>
-        <hr>
-        <br>
-          <validate tag="div">
-            <input 
-              type="text"
-              id="inputFondo"
-              name="inputFondo" 
-              class="form-control"
-              autocomplete="off"
-              v-model.trim="formData.fondo" 
-            />
-          </validate>
+      <p>Cantidad de palabras: {{ count() }}</p>
 
-          <h3>Size</h3>
-        <hr>
-        <br>
-          <validate tag="div">
-            <input 
-              type="text"
-              id="inputSize"
-              name="inputSize" 
-              class="form-control"
-              autocomplete="off"
-              v-model.trim="formData.size" 
-            />
-          </validate>
-
-          <h3>Enter text</h3>
-        <hr>
-        <br>
-          <validate tag="div">
-            <input 
-              type="text"
-              id="input"
-              name="input" 
-              class="form-control"
-              autocomplete="off"
-              v-model.trim="formData.input" 
-            />
-          </validate>
-
-          <Son  :input="formData.input"
-                :color = "formData.color"
-                :fondo = "formData.fondo"
-                :size = "formData.size"
-          ></Son>
         <Answers></Answers>
         <br>
       </div>
@@ -69,12 +22,11 @@
 </template>
 
 <script>
-import Son from './InputSon.vue'
 import Answers from './Answers.vue'
+
 export default {
   name: 'src-components-Input',
   components : {
-      Son,
       Answers
     },
   props: [],
@@ -83,21 +35,25 @@ export default {
   
   data () {
     return {
-      formData : this.getInicialData()
+      color: "",
+      fondo: "",
+      size: "",
+      texto: "Ingrese Texto Aqui",
+      
     }
   },
   methods: {
-      getInicialData() {
-        return {
-          input: '',
-          Color: '',
-          Fondo: '', 
-          Size: ''
-        }
-      },
+    estiloTexto(color, fondo, size) {
+      return `background: ${fondo}; color:  ${color}; width:  ${size}px `;
+    }
   },
+
   
   computed: {
+    count() {
+      let count = this.texto.split(" ").length;
+      return count;
+    },
   },
   
 }
